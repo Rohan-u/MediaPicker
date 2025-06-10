e8c352c62e003974dee686052b5fb8fa1387359e
rohan.u <rohan.u@vrinsoft.com>
2025-06-10 15:24:44 +0530

    Gradle Java version updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,12 +28,12 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_21
-        targetCompatibility = JavaVersion.VERSION_21
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
 
     kotlinOptions {
-        jvmTarget = "21"
+        jvmTarget = "11"
     }
     buildFeatures {
         dataBinding = true
